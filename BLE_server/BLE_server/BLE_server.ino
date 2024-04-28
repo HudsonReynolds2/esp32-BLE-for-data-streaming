9aa999ab8bfafbc672064e1533272499ae8e3f6b
Hudson Reynolds <hudsonre@bu.edu>
2024-04-28 11:47:49 +1000

    Finally working data streaming

--- BLE_server/BLE_server/BLE_server.ino
+++ BLE_server/BLE_server/BLE_server.ino
@@ -1,52 +1,59 @@
-/*
-    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleServer.cpp
-    Ported to Arduino ESP32 by Evandro Copercini
-    updates by chegewara
-*/
-
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
 
-// See the following for generating UUIDs:
-// https://www.uuidgenerator.net/
-
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
-static BLEServer *pServer = NULL; // Declare pServer globally
-static BLEService *pService = NULL;
-static BLECharacteristic *pCharacteristic = NULL; // Declare pTxCharacteristic globally
+BLECharacteristic* pTxCharacteristic = NULL; // Declare pTxCharacteristic globally
 
+// Function to generate random heart rate values
+float heartRateValues() {
+  return random(50, 201) + random(0, 100) / 100.0;
+}
 
 void setup() {
   Serial.begin(115200);
+  randomSeed(analogRead(0)); // For heartRateValues()
+
   Serial.println("Starting BLE work!");
 
-  BLEDevice::init("TestHeartRateBLE");
-  pServer = BLEDevice::createServer();
-  pService = pServer->createService(SERVICE_UUID);
-  pCharacteristic = pService->createCharacteristic(
-                                         CHARACTERISTIC_UUID,
-                                         BLECharacteristic::PROPERTY_READ |
-                                         BLECharacteristic::PROPERTY_WRITE
-                                       );
+  BLEDevice::init("TestWithRandomValues");
+  BLEServer *pServer = BLEDevice::createServer();
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  pTxCharacteristic = pService->createCharacteristic(
+    CHARACTERISTIC_UUID,
+    BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY
+  );
 
-  pCharacteristic->setValue("Hello World says Hudson");
+  // Set an initial value for the characteristic
+  pTxCharacteristic->setValue("Hello World says Neil");
+
+  // Start the service
   pService->start();
-  // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
-  static BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+
+  // Set up advertising
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->setScanResponse(true);
-  pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+  pAdvertising->setMinPreferred(0x06); // Functions that help with iPhone connections issue
   pAdvertising->setMinPreferred(0x12);
   BLEDevice::startAdvertising();
+
+  Serial.println("Characteristic defined! Now you can read it on your phone!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int randVal = random(50,201);
-  pCharacteristic->setValue(randVal);
-  pCharacteristic->notify();
-  delay(1000);
+  // Generate a random heart rate value
+  float randHeartRate = heartRateValues();
+  Serial.print("Random Heart Rate: ");
+  Serial.println(randHeartRate);
+
+  String stringValue = String(randHeartRate);
+
+  // Update the characteristic value
+  pTxCharacteristic->setValue(stringValue.c_str());
+  pTxCharacteristic->notify();
+
+  delay(1000); // Wait for 1 second
 }
